c95e859a4a216449d14b3a370c0a906763327374
vagrant <vagrant@homestead>
2018-11-09 13:16:42 +0000

    list topics created by user

---
+++ resources/views/users/_topics.blade.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lining
+ * Date: 2018/11/9
+ * Time: 5:52 PM
+ */
+?>
+@if (count($topics))
+
+    <ul class="list-group">
+        @foreach ($topics as $topic)
+            <li class="list-group-item">
+                <a href="{{ route('topics.show', $topic->id) }}">
+                    {{ $topic->title }}
+                </a>
+                <span class="meta pull-right">
+                {{ $topic->reply_count }} 回复
+                <span> ⋅ </span>
+                    {{ $topic->created_at->diffForHumans() }}
+            </span>
+            </li>
+        @endforeach
+    </ul>
+
+@else
+    <div class="empty-block">暂无数据 ~_~ </div>
+@endif
+
+{{-- 分页 --}}
+{!! $topics->render() !!}
