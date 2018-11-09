f22c7fc0fc5e5621e328ba590a34760887bec29b
vagrant <vagrant@homestead>
2018-11-09 09:00:18 +0000

    Seeding topics and users data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,10 +7,21 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+
+
+        $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+
+        $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $facker = app(\Faker\Generator::class);
+
+        $topics = factory(Topic::class)->times(100)->make()->each(function ($topic, $index) use ($user_ids, $category_ids , $facker) {
+
+            $topic->user_id     = $facker->randomElement($user_ids);
+
+            $topic->category_id = $facker->randomElement($category_ids);
+
         });
 
         Topic::insert($topics->toArray());
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(\Faker\Generator::class);
+
+        $avatars = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+        $users = factory(\App\Models\User::class)->times(10)->make()
+            ->each(function ($user , $index) use ($faker , $avatars){
+                // 从头像数组中随机取出一个并赋值
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password','remember_token'])->toArray();
+
+        \App\Models\User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = \App\Models\User::find(1);
+        $user->name = 'lining';
+        $user->email = 'lining8742@gmail.com';
+        $user->avatar = 'http://larabbs.two/uploads/images/avatar/201811/09/2_1541748504_dOy4dvMtwl.jpeg';
+        $user->save();
+
+    }
+}
